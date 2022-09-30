8fb889067943a4dbce439eff5503c8ab7f957be8
Simone De Matteis <Simone.De.Matteis-CIC@ibm.com>
2022-09-30 13:12:02 +0200

    fix: coverage, tests, ep aligned

--- src/test/java/it/finanze/sanita/fse2/ms/gtwstatuscheckms/EnumsTest.java
+++ src/test/java/it/finanze/sanita/fse2/ms/gtwstatuscheckms/EnumsTest.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ComponentScan(basePackages = {Constants.ComponentScan.BASE})
