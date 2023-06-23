9c8ffab80f19c159323b36f17d3ba4c361d7bf9c
Simone Le Noci <72795995+SimoneNuts@users.noreply.github.com>
2023-06-23 12:57:21 +0200

    feat(tests): ExceptionTest and JsonUtilityTest

---
+++ src/test/java/it/finanze/sanita/fse2/ms/gtwstatuscheckms/JsonUtilityTest.java
@@ -0,0 +1,57 @@
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ * 
+ * Copyright (C) 2023 Ministero della Salute
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package it.finanze.sanita.fse2.ms.gtwstatuscheckms;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import it.finanze.sanita.fse2.ms.gtwstatuscheckms.config.Constants;
+import it.finanze.sanita.fse2.ms.gtwstatuscheckms.utility.JsonUtility;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(Constants.Profile.TEST)
+class JsonUtilityTest {
+  
+
+    @Test
+    @DisplayName("jsonToObject OK")
+    void testJsonToObjectOk() {
+        String input = "{\"identificativo\":\"stub\",\"flagPresaInCarico\":false}";
+        Map<String, Object> map = new HashMap<>();
+        map.put("identificativo", "stub");
+        map.put("flagPresaInCarico", false);
+        assertEquals(map, JsonUtility.jsonToObject(input, Map.class));
+    }
+
+    @Test
+    @DisplayName("objectToJson malformedInput")
+    void testObjectToJsonMalformedInput() {
+        Map<String, Object> input = new HashMap<>();
+        input.put(null, null);
+        assertEquals("", JsonUtility.objectToJson(input));
+    }
+
+    @Test
+    @DisplayName("objectToJson OK")
+    void testObjectToJsonOK() {
+        Map<String, Object> input = new HashMap<>();
+        input.put("key", "value");
+        assertEquals("{\"key\":\"value\"}", JsonUtility.objectToJson(input));
+    }
+}
