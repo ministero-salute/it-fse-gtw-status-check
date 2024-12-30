598f15fda94f16908d92f3d2319ddfcf73b7a68d
Andrea De Lella <Andrea.De.Lella-CIC@ibm.com>
2024-12-30 16:00:28 +0100

    minor fix

--- src/main/java/it/finanze/sanita/fse2/ms/gtwstatuscheckms/enums/ActionRes.java
+++ src/main/java/it/finanze/sanita/fse2/ms/gtwstatuscheckms/enums/ActionRes.java
@@ -2,8 +2,8 @@
 // * SPDX-License-Identifier: AGPL-3.0-or-later
 // */
 ///*
- * SPDX-License-Identifier: AGPL-3.0-or-later
- * 
+ /* SPDX-License-Identifier: AGPL-3.0-or-later
+ *
  * Copyright (C) 2023 Ministero della Salute
  * 
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
